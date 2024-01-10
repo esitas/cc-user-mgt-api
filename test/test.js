9891635b10ea46195a77090bd66c3c1429722980
CORP\owton <peter.owton@dxcas.com>
2024-01-10 11:50:05 -0800

    added test for search endpoint

--- test/test.js
+++ test/test.js
@@ -12,10 +12,10 @@ describe("Users API", () => {
   it("GET /users/{userId} returns the user", async () => {
     const user = {
       userId: 1,
-      userFirstName: "Alice",
-      userLastName: "Smith",
-      workLocation: "San Francisco",
-      skills: ["Java", "C++"],
+      userFirstName: "John",
+      userLastName: "Doe",
+      workLocation: "New York",
+      skills: ["JavaScript", "Python"],
     };
     const res = await request(app).get("/users/1");
     expect(res.body).toEqual(user);
@@ -23,7 +23,6 @@ describe("Users API", () => {
 
   it("PUT /users creates a new user and returns a 200 response", async () => {
     const newUser = {
-      userId: 6,
       userFirstName: "John",
       userLastName: "Doe",
       workLocation: "New York",
@@ -34,13 +33,14 @@ describe("Users API", () => {
   });
 
   it("DELETE /users/${userId} deletes the user", async () => {
-    const users = await request(app).get("/users");
-    let index = users.body.slice(-1)[0].userId;
+    const res = await request(app).get("/users");
+    let index = res.body.length - 1;
+    const userId = res.body[index].userId;
 
-    const res = await request(app).delete("/users/" + index);
-    expect(res.status).toEqual(200);
+    const res2 = await request(app).delete("/users/" + userId);
+    expect(res2.status).toEqual(200);
 
-    const resAfterDelete = await request(app).delete("/users/" + index);
+    const resAfterDelete = await request(app).delete("/users/" + userId);
     expect(resAfterDelete.status).toEqual(404);
   });
 
@@ -63,6 +63,20 @@ describe("Users API", () => {
     //Check that api returns the new workLocation
     const res1 = await request(app).get("/users/12");
     expect(res1.body.workLocation).toBe("Toronto");
+  });
+
+  it("GET /search?${keyword} should search the firstName, lastName, workLocation and skills properties for the keyword and return an array of objects that match the criteria", async () => {
+    const res1 = await request(app).get("/search?keyword=John");
+    const length1 = res1.body.length;
+    expect(length1).toBeGreaterThan(0);
+
+    const res2 = await request(app).get("/search?keyword=Smith");
+    const length2 = res2.body.length;
+    expect(length2).toBeGreaterThan(0);
+
+    const res3 = await request(app).get("/search?keyword=java");
+    const length3 = res3.body.length;
+    expect(length3).toBeGreaterThan(0);
   });
 
   afterAll(async () => {
